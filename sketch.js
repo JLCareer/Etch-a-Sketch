a6d080b7d19e08f924dddc5749f374290c1b2fff
Jiajun Liang <Jiajunlwork@gmail.com>
2025-08-12 21:32:34 -0400

    Update .js and .css for button new grid
    Recode grid creation into function.
    Add button for new grid creation
    Create new grid by remove current grid and create new grid and append it

--- sketch.js
+++ sketch.js
@@ -1,23 +1,51 @@
-let gridRow = 100, gridColumn = 100;
-
 const body = document.querySelector("body")
 
 const gridContainer = document.createElement("div")
-
 gridContainer.classList.add("container");
-
 body.appendChild(gridContainer);
 
-for(let i = 0; i < gridRow; i++){
-    let row = document.createElement("div")
-    row.classList.add("row")
-    
-    for(let j = 0; j < gridColumn; j++){
-        let girdPixel = document.createElement("div")
-        girdPixel.setAttribute("class", "pixel")
-        row.appendChild(girdPixel)
-    }
-    
-    gridContainer.append(row)
-}
+function createGrid(size) {
+    for(let i = 0; i < size; i++){
+        let row = document.createElement("div")
+        row.classList.add("row")
+        
+        for(let j = 0; j < size; j++){
+            let girdPixel = document.createElement("div")
+            girdPixel.setAttribute("class", "pixel")
+            row.appendChild(girdPixel)
+        }
+        
 
+        document.querySelector(".container").append(row)
+    }
+};
+
+createGrid(16);
+
+gridContainer.addEventListener("mouseover", (event) => {
+    let target = event.target;
+    target.setAttribute("style", "background-color: black")
+})
+
+const sizeButton = document.createElement("button")
+sizeButton.textContent = "Size"
+body.insertBefore(sizeButton, gridContainer)
+
+sizeButton.addEventListener("click", () => {
+    let newSize = parseInt(prompt("Enter new grid size (1 ~ 100)"))
+
+    if(newSize > 0 && newSize <= 100){
+        document.querySelector(".container").remove();
+
+        let newgrid = document.createElement("div")
+        newgrid.classList.add("container");
+        body.appendChild(newgrid);
+
+        createGrid(newSize);
+
+        newgrid.addEventListener("mouseover", (event) => {
+            let target = event.target;
+            target.setAttribute("style", "background-color: black")
+        })
+    }
+})
